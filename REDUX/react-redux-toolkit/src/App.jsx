f1362d8fbe460e07dd519fb527c7da320a3ecae6
Saurabh <ssp94408@gmail.com>
2026-02-20 17:23:53 +0530

    Redux completed

---
+++ REDUX/react-redux-toolkit/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import {increment ,decrement,reset, incrementByAmount} from './features/counter/counterSlice'
+import './App.css'
+
+function App() {
+  const count = useSelector((state)=>state.counter.value);
+  const [amount ,setAmount]=useState(0)
+  const dispatch =useDispatch();
+
+  function handleIncrementClick(){
+       dispatch(increment());
+  }
+
+  function handleDecrementClick(){
+       dispatch(decrement()); 
+  }
+
+ function handleResetClick(){
+       dispatch(reset()); 
+  }
+   function handleIncAmountClick(){
+       dispatch(incrementByAmount(amount)); 
+  }
+
+  return (
+    <div className='container'>
+     <button onClick={handleIncrementClick}>+</button>
+     <p>Count:{count}</p>
+     <button onClick={handleDecrementClick}>-</button>
+     <br />
+     <br />
+       <button onClick={handleResetClick}>Reset</button>
+       <br /><br />
+       <input type="Number"
+       value={amount}
+       placeholder='Enter Amount'
+       onChange={(e)=>setAmount(e.target.value)} />
+        <br />
+     <br />
+       <button onClick={handleIncAmountClick}>Inc By Amt</button>
+
+    </div>
+  )
+}
+
+export default App
